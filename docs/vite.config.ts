fb1c112459af8ff97cce468f24685c5f68edaf36
TKSOHLT <kimisami31@gmail.com>
2025-12-30 10:06:34 -0600

    ✨Feat: Primera versión lista, desplegada en npm, desplegando documentación en github pages con github actions

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -6,9 +6,14 @@ import path from 'path';
 export default defineConfig({
   root: path.resolve(__dirname),
   plugins: [react(), tailwindcss()],
+  base: '/react-spreadsheet-cell/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '../src'),
     },
+  },
+  build: {
+    outDir: path.resolve(__dirname, '../dist-docs'),
+    emptyOutDir: true,
   },
 });
